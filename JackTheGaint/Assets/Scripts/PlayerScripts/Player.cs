e517d31da6e690403e8fbcadf4caa2230ccd2e5c
suyashmadhesia <suyashmadhesia@gmail.com>
2020-11-06 17:14:31 +0530

    playerScript

---
+++ JackTheGaint/Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8.0f;
+	public float maxVelocity = 4.0f;
+	private Rigidbody2D myBody;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		PlayerMovement();
+		
+ 	}
+
+	void PlayerMovement(){
+		float forceX = 0f;
+		float vel = Mathf.Abs(myBody.velocity.x);
+
+		float h = Input.GetAxisRaw("Horizontal");
+
+		if (h > 0){
+
+			if (vel < maxVelocity){
+				forceX = speed;
+			}
+				
+			anim.SetBool("walk", true);
+			Vector3 scale = transform.localScale;
+			scale.x = 1;
+			transform.localScale = scale;
+
+		}else if(h < 0){
+
+			if (vel < maxVelocity){
+				forceX = -speed;
+			}
+
+			anim.SetBool("walk", true);
+			Vector3 scale = transform.localScale;
+			scale.x = -1;
+			transform.localScale = scale;	
+		
+		}
+		else
+		{
+			anim.SetBool("walk", false);
+		}
+		myBody.AddForce(new Vector2(forceX, 0));
+
+	}
+
+}
