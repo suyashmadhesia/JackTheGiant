d4bef5e7a58e9ccf38f942f6d256229cf3564a3d
suyashmadhesia <suyashmadhesia@gmail.com>
2020-11-06 23:59:38 +0530

    BgAndCloudCollector

---
+++ JackTheGaint/Assets/Scripts/Cloud Collectors/CloudSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] clouds;
+	private float distanceBetweenClouds = 3f;
+
+	private float minX;
+	private float maxX;
+	private float  lastCloudPositionY;
+
+	private float controlX;
+	[SerializeField]
+	private GameObject[] collectables;
+
+	private GameObject player;
+
+	private void Start() {
+		
+	}
+
+	private void Update() {
+		
+	}
+
+}
