1c8d2a4f030c5d650599cab03c6b13f195f30957
suyashmadhesia <suyashmadhesia@gmail.com>
2020-11-09 15:56:39 +0530

    AllNavigationsAndUIcompleted

---
+++ JackTheGaint/Assets/Scripts/GameController/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("GameplayScene");
+	}
+
+	public void GoToHighScoreMenu(){
+		SceneManager.LoadScene("HighscoreScene");
+	}
+
+	public void GotoOptionMenu(){
+		SceneManager.LoadScene("OptionScene");
+	}
+
+	public void QuitScene(){
+		Application.Quit();
+	}
+
+	public void MusicButton(){
+		
+	}
+
+}
---
+++ JackTheGaint/Assets/Scripts/GameController/OptionMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionMenuController : MonoBehaviour {
+
+
+	void Start () {
+
+	}
+
+	public void GoBackButtonInOption(){
+		SceneManager.LoadScene("MainMenu");
+	}
+
+}
